15cef0cb8d0f5abaa8d19541cab4e20cc8e759db
Userkaylin <kaylz.chups@gmail.com>
2024-09-06 09:45:18 +0500

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "railway",
+        'USER': "postgres",
+        'PASSWORD': "OrIBXiCSlexRlDIdXoaYzrbRPcomjgqm",
+        'HOST': "meticulous-empathy.railway.internal",
+        'PORT': "5432",
     }
 }
 AUTH_USER_MODEL = 'app.CustomUser'
